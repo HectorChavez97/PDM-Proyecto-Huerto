ffb180f396fe2a168d42b75c78c67b9d59b660a5
luishdezh <43009665+luishdezh@users.noreply.github.com>
2019-11-01 22:01:58 -0600

    Add Activities

---
+++ PDMHuerto/app/src/main/java/com/example/pdmhuerto/Activities/Detail_Activity.kt
@@ -0,0 +1,13 @@
+package com.example.pdmhuerto.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pdmhuerto.R
+
+class Detail_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
